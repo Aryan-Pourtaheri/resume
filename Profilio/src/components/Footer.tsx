1d1940f8ea1567b73e829265923ba135682e14f4
Aryan-Pourtaheri <3programm.6aagm9@gmail.com>
2025-06-23 13:34:01 +0300

    changed profilio to portfilio

--- Profilio/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-gray-800 text-white p-4 text-center">
-      <p>© 2024 My Portfolio. All Rights Reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
